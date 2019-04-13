48f626d44bd56feae48f9901d75bac415a59b7fa
johnklee <puremonkey2001@yahoo.com.tw>
2019-04-13 13:07:18 +0800

    Initialize source code

---
+++ fpu/flist.py
@@ -0,0 +1,360 @@
+import functools
+import collections
+
+from abc import ABCMeta, abstractmethod
+try:
+    from fp import *
+except:
+    from .fp import *
+
+
+ret = TailCall.ret
+sus = TailCall.sus
+
+class UOException(Exception):
+    r'''
+    Unsupported Operation Exception
+    '''
+    def __init__(self, message, errors=None):
+        super(Exception, self).__init__(message)
+        self.errors = errors
+
+class List:
+    __metaclass__ = ABCMeta
+    
+    @abstractmethod
+    def head(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def tail(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def isEmpty(self):
+        r'''
+        * Nil object: False
+        * Cons object: True
+
+        @return
+            True if Cons object; False otherwise.
+        '''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def setHead(self, h):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def drop(self, n):
+        r'''
+        Dropping the n first elements of a list while not mutating or creating anything
+        '''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def dropWhile(self, f):
+        r'''
+        Method to remove elements from the head of the List as long as a condition holds true.
+        '''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def reverse(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def init(self):
+        r'''
+        Remove the last element from a list.
+        '''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def size(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def length(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def map(self, f):
+        r'''
+        Modify each element of a list by applying a specified function to it
+
+        @return
+            List object
+        '''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def flatMap(self, f):
+        r'''
+        method that applies to each element of List<A> a function from A to List<B>, and returns a List<B>
+
+        @return
+            List object
+        '''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def filter(self, f):
+        r'''
+        Method that removes from a list the elements that don't satisfy a given predicate
+        '''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def append(self, e):
+        raise NotImplementedError()
+    
+    def reduce(self, f, identity=None):
+        if identity is not None:
+            return self.foldLeft(identity, f)
+        else:
+            return self.t.foldLeft(self.h, f)
+
+    @abstractmethod
+    def foldRight(self, identity, f):
+        raise NotImplementedError()
+
+    def foldLeft(self, identity, f):
+        return self._foldLeft(identity, self, f).eval()
+
+    def _foldLeft(self, acc, alist, f):
+        return ret(acc) if alist.isEmpty() else sus(Supplier(self._foldLeft, f(acc, alist.h), alist.t, f))
+
+    @abstractmethod
+    def headOption(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def exists(self, f):
+        raise NotImplementedError()
+
+    def cons(self, e):
+        return Cons(e, self)
+
+    def __iter__(self):
+        return ListIter(self)
+
+
+class ListIter:
+    def __init__(self, cont):
+        self.cont = cont;
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.cont.length() == 0:
+            raise StopIteration()
+        else:
+            val = self.cont.head()
+            self.cont = self.cont.tail()
+            return val
+
+    def next(self):
+        return self.__next__()
+
+
+class Nil(List):
+    def __init__(self):
+        pass
+
+    def head(self):
+        raise UOException('head called on an empty list')
+
+    def tail(self):
+        raise UOException('tail called on an empty list')
+
+    def isEmpty(self):
+        return True
+
+    def setHead(self, h):
+        raise UOException('setHead called on an empty list')
+
+    def __str__(self):
+        return "[NIL]"
+
+    def drop(self, n):
+        return self
+
+    def dropWhile(self, f):
+        return self
+
+    def reverse(self):
+        return self
+
+    def init(self):
+        raise UOException('init called on an empty list')
+
+    def size(self):
+        raise UOException('size called on an empty list')
+
+    def length(self):
+        return 0
+
+    def map(self, f):
+        return self
+
+    def flatMap(self, f):
+        return self
+
+    def filter(self, f):
+        return self
+
+    def append(self, a):
+        return None
+
+    def foldRight(self, identity, f):
+        raise UOException('foldRight called on an empty list')
+
+    def headOption(self):
+        raise UOException('headOption called on an empty list')
+
+    def exists(self, f):
+        return False
+
+
+class Cons(List):
+    def __init__(self, head, tail=None):
+        self.h = head
+        self.t = tail
+        if tail:
+            self.l = self.t.length() + 1
+        else:
+            self.l = 1
+
+    def head(self):
+        return self.h
+
+    def tail(self):
+        return self.t
+
+    def isEmpty(self):
+        return False
+
+    def setHead(self, h):
+        return Cons(h, self.t)
+
+    def __str__(self):
+        return "[{}NIL]".format(self.toString("", self).eval())
+
+    def toString(self, sb, alist):
+        return ret(sb) if alist.isEmpty() else sus(Supplier(self.toString, sb + "{}, ".format(alist.h), alist.t))
+
+    def drop(self, n):
+        return self._drop(self, n).eval()
+
+    def _drop(self, alist, n):
+        return ret(alist) if n <=0 or alist.isEmpty() else sus(Supplier(self._drop, alist.t, n - 1))
+
+    def dropWhile(self, f):
+        return self._dropWhile(self, f).eval()
+
+    def _dropWhile(self, alist, f):
+        return sus(Supplier(self._dropWhile, alist.t, f)) if not alist.isEmpty() and f(alist.h) else ret(alist)
+
+    def reverse(self):
+        return self._reverse(nil, self).eval()
+
+    def _reverse(self, acc, alist):
+        return ret(acc) if alist.isEmpty() else sus(Supplier(self._reverse, Cons(alist.h, acc), alist.t))
+
+    def init(self):
+        return self.reverse().tail().reverse()
+
+    def size(self):
+        return 1 if self.t.isEmpty() else 1 + self.t.size()
+
+    def length(self):
+        return self.l
+
+    def map(self, f):
+        return self.foldRight(nil, lambda h, t: Cons(f(h), t))
+
+    def flatMap(self, f):
+        return self.foldRight(fl(), lambda e, a: concat(f(e), a))
+
+    def filter(self, f):
+        return self.foldRight(fl(), lambda e, a: Cons(e, a) if f(e) else a)
+
+    def append(self, a):
+        return None
+
+    def foldRight(self, identity, f):
+        return self._foldRight(identity, self.reverse(), identity, f).eval()
+
+    def _foldRight(self, acc, alist, identity, f):
+        return ret(acc) if alist.isEmpty() else sus(Supplier(self._foldRight, f(alist.h, acc), alist.t, identity, f))
+
+    def headOption(self):
+        raise UOException('headOption called on an empty list')
+
+    def exists(self, f):
+        r'''
+        @Need to be enhanced by fold operation
+        '''
+        return f(self.head()) or self.tail().exists(f)
+
+
+nil = Nil()
+
+
+###################################
+# Common Utility Function
+###################################
+
+def fl(*args):
+    r'''
+    Create List with element as given input
+
+    @param flat(bool):
+        True to flatten the element as list (First level only)
+    '''
+    if len(args) == 0:
+        return nil
+    else:
+        sn = nil
+        for i in range(len(args)):
+            e = args[-1*(i+1)]
+            if isinstance(e, list):
+                for se in e:
+                    sn = Cons(se, sn)
+            elif isinstance(e, range):
+                for se in e:
+                    sn = Cons(se, sn)
+            else:
+                sn = Cons(args[-1*(i+1)], sn)
+        return sn
+
+def concat(list1, list2):
+    r'''
+    Concatenate input two list
+    '''
+    return list1 if list2.isEmpty() else _concat(list1.reverse(), list2).eval()
+
+def _concat(list1, list2):
+    return ret(list2) if list1.isEmpty() else sus(Supplier(_concat, list1.t, Cons(list1.h, list2)))
+
+def fsum(alist):
+    r'''
+    * For an empty list: 0 
+    * For a non-empty list: head plus the sum of the tail
+    '''    
+    return 0 if alist.isEmpty() else alist.foldRight(0, lambda e, a: e + a)
+
+def fproduct(alist):
+    r'''
+    * For an empty list: 1.0 
+    * For a non-empty list: head * product of tail
+    '''
+    return 1.0 if alist.isEmpty() else alist.foldRight(1.0, lambda e, a: e * a)
+
+def flatten(alist):
+    r'''
+    Method for flattening a list of lists into a list containing all elements of each contained list
+    '''
+    return alist.foldRight(fl(), lambda e, a: concat(e, a))
---
+++ fpu/fp.py
@@ -0,0 +1,484 @@
+#!/usr/bin/env python3
+import functools
+import logging
+from abc import ABCMeta, abstractmethod
+#from .utils import *
+try:
+    from utils import *
+except:
+    from .utils import *
+
+r'''
+This module is created to let Python support FP (Function Programming)
+'''
+
+################################
+# API Segment
+################################
+def compose2(f, g):
+    def m(*args):
+        return f(g(*args))
+    return m
+    #return lambda x: f(g(x))
+
+def compose(*functions):
+    def m(*args):
+        return args
+    #return functools.reduce(compose2, functions, lambda x:x)
+    return functools.reduce(compose2, functions, m)
+
+def andThen(f, g):
+    def m(*args):
+        return g(f(*args))
+    return m
+    #return lambda x: g(f(x))
+
+
+################################
+# Class Segment
+################################
+@aliased
+class Option:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def map(self, func):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def getOrElse(self, supr):
+        r'''
+        Method that will return either the contained value if it exists, 
+        or return the result by executing the given `supr` object.
+        '''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def getOr(self, dft):
+        r'''
+        This method that will return either the contained value if it exists, 
+        or a provided default one otherwise.
+        '''
+        raise NotImplementedError()
+
+    @abstractmethod
+    def isSome(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def getOrThrow(self):
+        raise NotImplementedError()
+
+    @staticmethod
+    def some(value):
+        return Some(value)
+
+    @staticmethod
+    def none():
+        return N()
+
+    @alias('fm')
+    def flatMap(self, func):
+        return self.map(func).getOrElse(Option.none)
+
+    def orElse(self, supr):
+        return self.map(lambda e: self).getOrElse(supr)
+
+    def filter(self, func):
+        return self.flatMap(lambda e: self if func(e) else Option.none())
+
+
+class N(Option):
+    def map(self, func):
+        return self
+
+    def getOr(self, dft):
+        return dft
+
+    def getOrElse(self, supr):
+        return supr()
+
+    def isSome(self):
+        return False
+
+    def getOrThrow(self):
+        raise Exception('get called on None')
+
+    def __eq__(self, other):
+        return isinstance(other, N)
+
+    def __str__(self):
+        return "None()"
+
+    def __hash__(self):
+        return hash(0)
+
+
+class Some(Option):
+    def __init__(self, value):
+        self.value = value
+
+    def map(self, func):
+        return Some(func(self.value))
+
+    def getOr(self, dft):
+        return self.value
+
+    def getOrElse(self, supr):
+        return self.value
+
+    def isSome(self):
+        return True
+
+    def getOrThrow(self):
+        return self.value
+
+    def __eq__(self, other):
+        return True if isinstance(other, Some) and self.value == other.value or self.value == other else False
+
+    def __str__(self):
+        return "Some({})".format(self.value)
+
+    def __hash__(self):
+        return hash(self.value)
+
+
+class Supplier:
+    def __init__(self, func, *args):
+        self.func = func
+        self.args = args
+
+    def __call__(self):
+        return self.func(*self.args)
+
+
+class Case:
+    def __init__(self, cond, rst):
+        r'''
+        Constructor
+
+        @param cond(Callable):
+            Condition object
+        @param rst(Callable):
+            Clause to execute after matching
+        '''
+        self.cond = cond    # Callable
+        self.rst = rst      # Callable
+
+    @staticmethod
+    def mcase(cond, rst):
+        r'''
+        Conditional clause
+        '''
+        return Case(cond, rst)
+
+    @staticmethod
+    def default(rst):
+        r'''
+        Default clause
+        '''
+        return Case(None, rst)
+
+    @staticmethod
+    def match(dftCase, *matches):
+        r'''
+        Perform matching clause
+        '''
+        for m in matches:
+            if m.cond is None:
+                return m.rst() if callable(m.rst) else m.rst
+            elif m.cond():
+                return m.rst() if callable(m.rst) else m.rst
+        return dftCase.rst() if callable(dftCase.rst) else dftCase.rst
+
+
+class Effect:
+    r'''
+    Abstract class to be implemented as functional object.
+    '''
+    __metaclass__ = ABCMeta
+
+    def __call__(self, value):
+        self.call(value)
+
+    @abstractmethod
+    def call(self, value):
+        raise NotImplementedError()
+
+
+class Result:
+    __metaclass__ = ABCMeta
+
+    @staticmethod
+    def failure(message):
+        return Failure(message)
+
+    @staticmethod
+    def success(value=None):
+        return Success(value)
+
+    @staticmethod
+    def empty():
+        return Empty()
+
+    @abstractmethod
+    def bind(self, successEff, failureEff):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def getOrElse(defaultVal):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def map(func):
+        raise NotImplementedError()
+
+
+class Empty(Result):
+    def __init__(self):
+        pass
+
+    def bind(self, successEff, failureEff):
+        return failureEff('Empty')
+
+    def __str__(self):
+        return "Empty()"
+
+    def getOrElse(self, defaultVal):
+        if isinstance(defaultVal, Supplier):
+            return defaultVal()
+        else:
+            return defaultVal
+
+    def map(self, func):
+        return self
+
+class Success(Result):
+    def __init__(self, value):
+        self.value = value
+
+    def bind(self, successEff, failureEff):
+        return successEff(self.value)
+
+    def getOrElse(self, defaultVal):
+        return self.value
+
+    def map(self, func):
+        try:
+            return Success(func(self.value))
+        except:
+            logging.exception('Fail to apply {}'.format(func))
+            return Failure('Error while applying {}'.format(func))
+
+    def __str__(self):
+        return "Success({})".format(self.value)
+
+
+class Failure(Result):
+    def __init__(self, message):
+        self.message = message
+
+    def bind(self, successEff, failureEff):
+        return failureEff(self.message)
+    
+    def __str__(self):
+        return "Failure({})".format(self.message)
+
+    def getOrElse(self, defaultVal):
+        if isinstance(defaultVal, Supplier):
+            return defaultVal()
+        else:
+            return defaultVal
+
+    def map(self, func):
+        return self
+
+
+class TailCall:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def resume(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def eval(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def isSuspend(self):
+        raise NotImplementedError()
+
+    @staticmethod
+    def ret(t):
+        return Return(t)
+
+    @staticmethod
+    def sus(resume):
+        return Suspend(resume)
+
+
+class Suspend(TailCall):
+    def __init__(self, resume):
+        self.resume = resume
+
+    def isSuspend(self):
+        return True
+
+    def resume(self):
+        return self.resume()
+
+    def eval(self):
+        tc = self
+        while tc.isSuspend():
+            tc = tc.resume()
+        return tc.eval()
+
+
+class Return(TailCall):
+    def __init__(self, t):
+        self.t = t
+
+    def isSuspend(self):
+        return False
+
+    def resume(self):
+        raise NotImplementedError()
+
+    def eval(self):
+        return self.t
+
+
+class Fun:
+    def __init__(self, f):
+        self.f = f
+
+    def compose(self, of):
+        return Fun(compose(self, of))
+
+    def andThen(self, of):
+        return Fun(compose(of, self))
+
+    def __call__(self, *args):
+        return self.f(*args)
+
+
+class CollectionUtils:
+    r'''
+    Collection utilities used in FP
+    '''
+    # @staticmethod
+    # def l():
+    #    return []
+
+    @staticmethod
+    def l(*args):        
+        alist = []
+        alist.extend(args)
+        return alist
+
+    @staticmethod
+    def fl(tlist):
+        alist = []
+        alist.extend(tlist)
+        return tuple(alist)
+
+    @staticmethod
+    def m(alist, fun):
+        nlist = CollectionUtils.l()
+        for e in alist:
+            nlist.append(fun(e))
+        return tuple(nlist)
+   
+    @staticmethod
+    def head(alist):
+        if len(alist) == 0:
+            raise Exception('Input list with length as 0')
+        return CollectionUtils.l(alist[0])
+
+
+    @staticmethod
+    def copy(alist):
+        nlist = CollectionUtils.l()
+        nlist.extend(alist) 
+        return tuple(nlist)
+
+    @staticmethod
+    def tail(alist):
+        if len(alist) == 0:
+            raise Exception('Input list with length as 0')
+        nlist = list(CollectionUtils.copy(alist))
+        del nlist[0]
+        return tuple(nlist)
+
+    @staticmethod
+    def append(alist, e):
+        nlist = list(CollectionUtils.copy(alist))
+        nlist.append(e)
+        return tuple(nlist)
+
+    @staticmethod
+    def first(alist, fun):
+        for e in alist:
+            if fun(e):
+                return CollectionUtils.l(e)
+        return CollectionUtils.l()
+
+    @staticmethod
+    def foldLeft(alist, identity, fun):
+        rst = identity
+        for e in alist:
+            rst = fun(rst, e)
+        return rst
+
+    @staticmethod
+    def foldRight(alist, identity, fun):
+        rst = identity
+        for e in reversed(alist):
+            rst = fun(e, rst)
+
+        return rst
+
+    @staticmethod
+    def reverse(alist):
+        nlist = CollectionUtils.l()
+        for e in reversed(alist):
+            nlist.append(e)
+
+        return tuple(nlist)
+
+    @staticmethod
+    def prepend(alist, e):
+        nlist = CollectionUtils.copy(alist)
+        nlist.insert(0, e)
+        return nlist
+
+    @staticmethod
+    def forEach(alist, fun):
+        for e in alist:
+            fun(e)
+       
+    @staticmethod
+    def unfold(seed, f, p):
+        r'''
+        @param seed:
+            The input argument of second and third callable object
+        @param f(callable):
+            Generate the next value for collection
+        @param p(callable):
+            Callable to terminate the process by returning True
+        '''
+        nlist = CollectionUtils.l()
+        temp = seed
+        while p(temp):
+            nlist.append(temp)
+            temp = f(temp)
+ 
+        return tuple(nlist)
+
+    @staticmethod
+    def range(start, end, step=1, e_inclusive=False):
+        if e_inclusive:
+            return CollectionUtils.unfold(start, lambda e: e + step, lambda e: e <= end)
+        else:
+            return CollectionUtils.unfold(start, lambda e: e + step, lambda e: e < end)
