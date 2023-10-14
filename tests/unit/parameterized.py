d10cecebf4efe8da0e02ac7178f777e494edf849
johnkclee <johnkclee@google.com>
2023-10-14 11:53:59 +0800

    implement #57

--- tests/unit/parameterized.py
+++ tests/unit/parameterized.py
@@ -1,4 +1,4 @@
-# Copyright 2017 The Abseil Authors.
+# Copyright 2023 The FPU Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
